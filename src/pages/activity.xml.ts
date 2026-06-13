2e529b840cddc4ec0ba60809052ec14e3aabc260
AAIC <aaic@AAICs-MacBook-Air.local>
2026-06-13 17:14:26 +0530

    Configure ggnagarkar.com domain

--- src/pages/activity.xml.ts
+++ src/pages/activity.xml.ts
@@ -3,7 +3,7 @@ import activity from '../content/activity.json';
 import type { APIContext } from 'astro';
 
 export async function GET(context: APIContext) {
-  const site = context.site ?? 'https://www.gunanandnagarkar.com';
+  const site = context.site ?? 'https://ggnagarkar.com';
   const sortedActivity = [...activity].sort((a, b) => +new Date(b.date) - +new Date(a.date));
 
   return rss({
